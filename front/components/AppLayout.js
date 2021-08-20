fe04c0f5885006c62d2bba1c34d0563759dcb687
2-NOW <kj109888@gmail.com>
2021-08-21 02:16:03 +0900

    login form

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
+import LoginForm from '../components/LoginForm';
+import UserProfile from '../components/UserProfile';
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode="horizontal">
@@ -27,7 +32,7 @@ const AppLayout = ({ children }) => {
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽 메뉴
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
---
+++ front/components/UserProfile.js
@@ -0,0 +1,5 @@
+const UserProfile = () => {
+  return <div></div>;
+};
+
+export default UserProfile;
