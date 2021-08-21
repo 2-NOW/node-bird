04d7233a1d114301a9b9fc984a7f26336445010c
2-NOW <kj109888@gmail.com>
2021-08-21 20:26:17 +0900

    profile

---
+++ front/components/FollowingList.js
@@ -0,0 +1,5 @@
+const FollowingList = () => {
+  return <div></div>;
+};
+
+export default FollowingList;
--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,13 +1,31 @@
 import AppLayout from '../components/AppLayout';
 import Head from 'next/head';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowerList from '../components/FollowerList';
+import FollowingList from '../components/FollowingList';
 
 const Profile = () => {
+  const followingList = [
+    { nickname: '2NOW' },
+    { nickname: '사과' },
+    { nickname: '딸기' },
+  ];
+  const followerList = [
+    { nickname: '떡볶이' },
+    { nickname: '피자' },
+    { nickname: '감자' },
+  ];
+
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <AppLayout>내 프로필</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowingList header="팔로잉 목록" data={followingList} />
+        <FollowerList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
