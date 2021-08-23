625ec661ded4fc0dc2f728b5a317ef5dc0ddd664
2-NOW <kj109888@gmail.com>
2021-08-23 18:13:14 +0900

    install next-redux-wrapper & create store

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
+import wrapper from '../store/configureStore';
 
 const App = ({ Component }) => {
   return (
@@ -18,4 +19,4 @@ App.propTypes = {
   Component: PropTypes.elementType.isRequired,
 };
 
-export default App;
+export default wrapper.withRedux(App);
