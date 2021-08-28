c184726d338645fd4b45903f6c3d9b1d284f5b54
2-NOW <kj109888@gmail.com>
2021-08-28 19:12:05 +0900

    PostForm

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,9 +1,18 @@
 import AppLayout from '../components/AppLayout';
+import { useSelector } from 'react-redux';
+import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
 
 const Home = () => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+  const { mainPosts } = useSelector((state) => state.post);
+
   return (
     <AppLayout>
-      <div>Hello, Next !</div>
+      {isLoggedIn && <PostForm />}
+      {mainPosts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </AppLayout>
   );
 };
